2fea720cdac4a1400f190cd3ff950d1ac3f4b682
caspe <caspe@wp.pl>
2025-11-23 13:21:07 +0100

    Update service worker domain and zoneId

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10216151
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+    "domain": "gleeful-manatee-69543e.netlify.app",
+    "zoneId": 3119681
+};
+self.lary = "";
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
