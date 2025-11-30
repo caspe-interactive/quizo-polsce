aec44d882a8f4db1a590b3bf308ee14b2f9bb6d1
caspe <caspe@wp.pl>
2025-11-30 21:27:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
+    "domain": "quizo-polsce.onrender.com",
     "zoneId": 10241780
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+};
+self.lary = "";
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
