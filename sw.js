79921af03dee6950db66b116c920a731aa689503
caspe <caspe@wp.pl>
2025-12-05 21:55:40 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.options = { 
-    "domain": "quizo-polsce.onrender.com",
-    "zoneId": 3126911   // ID serwisu z Monetag
-};
-self.lary = "";
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
