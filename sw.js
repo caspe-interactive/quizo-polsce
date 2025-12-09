08ac96dc896634c5b1777fb9f79191e965f1053b
caspe <caspe@wp.pl>
2025-12-09 18:35:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  // Wszystkie żądania przepuszczamy, quiz działa normalnie
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('fetch', e => {});
